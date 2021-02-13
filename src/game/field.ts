8420c60b5e2490ff108c2746771774fe0cea1974
AXwain <5733365+AXwain@users.noreply.github.com>
2021-02-13 06:08:01 -0500

    Storing field's bubbles on an array

--- src/game/field.ts
+++ src/game/field.ts
@@ -13,6 +13,7 @@ export interface FieldConfiguration {
 export class Field {
   private readonly _field: Container
   private readonly _bubbleRadius: number
+  private readonly _bubbles: StaticBubble[][]
   private readonly _columns: number
   private readonly _rowHeight: number
   private readonly _rowShift: number
@@ -21,6 +22,7 @@ export class Field {
     this._field = new Container()
     this._field.x = configuration.x
     this._field.y = configuration.y
+    this._bubbles = []
     this._bubbleRadius = configuration.bubbleRadius
     this._columns = configuration.columns
     this._rowHeight = configuration.rowHeight
@@ -34,11 +36,16 @@ export class Field {
     let y = this._bubbleRadius
     let isShifted = shifted
     let count = 0
+
     const maxOnRow = (shifted: boolean): number => {
       return shifted ? this._columns - 1 : this._columns
     }
+
+    this._bubbles.length = 0
+    let currentRow: StaticBubble[] = []
     for (const colorIndex of stage) {
       const bubble = new StaticBubble(x, y, this._bubbleRadius, colorIndex)
+      currentRow.push(bubble)
       x += this._bubbleRadius * 2
       count++
       if (count === maxOnRow(isShifted)) {
@@ -46,6 +53,8 @@ export class Field {
         x = this._rowShift * (isShifted ? 1 : 0) + this._bubbleRadius
         y += this._rowHeight
         count = 0
+        this._bubbles.push(currentRow)
+        currentRow = []
       }
 
       bubble.setContainer(this._field)
