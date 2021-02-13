6629e4b9013a56def1620472dc553e091794905d
AXwain <5733365+AXwain@users.noreply.github.com>
2021-02-13 08:04:04 -0500

    Implemented Search Bubble Index with tests

--- src/game/field.ts
+++ src/game/field.ts
@@ -17,6 +17,7 @@ export class Field {
   private readonly _columns: number
   private readonly _rowHeight: number
   private readonly _rowShift: number
+  private _startsShifted: boolean
 
   constructor (configuration: FieldConfiguration, stage: number[], startsShifted = false) {
     this._field = new Container()
@@ -27,23 +28,43 @@ export class Field {
     this._columns = configuration.columns
     this._rowHeight = configuration.rowHeight
     this._rowShift = configuration.rowShift
+    this._startsShifted = startsShifted
 
     this.setup(stage, startsShifted)
   }
 
-  get container () {
+  get container (): Container {
     return this._field
   }
 
-  get totalRows () {
+  get totalRows (): number {
     return this._bubbles.length
   }
 
-  maxColumns(isShifted: boolean): number {
+  maxColumns (isShifted: boolean): number {
     return isShifted ? this._columns - 1 : this._columns
   }
 
+  searchBubbleIndex (x: number, y: number): number[] {
+    if (y >= 0 && x >= 0 && x <= this._columns * this._bubbleRadius * 2) {
+      const Row = Math.floor(y / this._rowHeight)
+      if (Row < this.totalRows) {
+        const RowIsShifted = Row % 2 === (this._startsShifted ? 0 : 1)
+        const ShiftPosition = RowIsShifted ? this._rowShift : 0
+        const Column = Math.floor((x - ShiftPosition) / (this._bubbleRadius * 2))
+
+        if (Column < this.maxColumns(RowIsShifted)) {
+          return [Row, Column]
+        }
+      }
+    }
+
+    return []
+  }
+
   setup (stage: number[], startsShifted = false): void {
+    this._startsShifted = startsShifted
+
     let x = this._bubbleRadius
     let y = this._bubbleRadius
     let isShifted = startsShifted
--- src/game/tests/field.test.ts
+++ src/game/tests/field.test.ts
@@ -6,11 +6,11 @@ describe('field', () => {
 
   const TestStage: number[] = [
     0, 0, 1, 1,
-      0, 1, 1,
+    0, 1, 1,
     2, 2, 3, 3,
-      2, 2, 3
+    2, 2, 3
   ]
-  
+
   const TestConfiguration: FieldConfiguration = {
     bubbleRadius: 8,
     columns: 4,
@@ -20,18 +20,53 @@ describe('field', () => {
     y: 0
   }
 
+  const GetField = (): Field => new Field(TestConfiguration, TestStage)
+
   it('should have a total of rows', () => {
+    const InputField = GetField()
     const ExpectedTotalRows = 4
-    const InputField = new Field(TestConfiguration, TestStage)
 
     expect(InputField.totalRows).toBe(ExpectedTotalRows)
   })
 
   it('should have a maximum of columns', () => {
-    const InputField = new Field(TestConfiguration, TestStage)
+    const InputField = GetField()
     const ExpectedMaxColumnsWhenShifted = TestConfiguration.columns - 1
 
     expect(InputField.maxColumns(IsNotShifted)).toBe(TestConfiguration.columns)
     expect(InputField.maxColumns(IsShifted)).toBe(ExpectedMaxColumnsWhenShifted)
+  })
+
+  describe('search bubble', () => {
+    it('should not find any bubble index', () => {
+      const InputField = GetField()
+      const OutsideY = TestConfiguration.rowHeight * InputField.totalRows + 1
+
+      expect(InputField.searchBubbleIndex(-1, 0)).toEqual([])
+      expect(InputField.searchBubbleIndex(0, -1)).toEqual([])
+      expect(InputField.searchBubbleIndex(0, OutsideY)).toEqual([])
+    })
+
+    it('should return top-left bubble index', () => {
+      const InputField = GetField()
+      const ExpectedIndex = [0, 0]
+      const Bottom = TestConfiguration.rowHeight - 1
+      const Right = TestConfiguration.bubbleRadius * 2 - 1
+
+      expect(InputField.searchBubbleIndex(0, 0)).toEqual(ExpectedIndex)
+      expect(InputField.searchBubbleIndex(Right, Bottom)).toEqual(ExpectedIndex)
+    })
+
+    it('should return bottom-right bubble index', () => {
+      const InputField = GetField()
+      const ExpectedIndex = [InputField.totalRows - 1, TestConfiguration.columns - 2]
+      const Top = TestConfiguration.rowHeight * (InputField.totalRows - 1)
+      const Bottom = Top + TestConfiguration.rowHeight - 1
+      const Left = TestConfiguration.bubbleRadius * 2 * (TestConfiguration.columns - 2) + TestConfiguration.rowShift
+      const Right = TestConfiguration.bubbleRadius * 2 + Left - 1
+
+      expect(InputField.searchBubbleIndex(Left, Top)).toEqual(ExpectedIndex)
+      expect(InputField.searchBubbleIndex(Right, Bottom)).toEqual(ExpectedIndex)
+    })
   })
 })
